8f26fcce69404ffbaf5bed0d0d8948b73a3b4f60
Joe <spindleshanksspider@gmail.com>
2024-10-20 18:52:31 +0800

    玩家状态机的创建

---
+++ Assets/Scripts/Player/state/PlayerState.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState
+{
+    protected PlayerStateMachine stateMachine;
+    protected Player player;
+
+    private string animBoolName;
+
+    public PlayerState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName)
+    {
+        this.stateMachine = _stateMachine;
+        this.player = _player;
+        this.animBoolName = _animBoolName;
+    }
+
+    public virtual void Enter()
+    {
+        Debug.Log("I enter " + animBoolName);
+    }
+
+    public virtual void update()
+    {
+        Debug.Log("I'm in  " + animBoolName);
+    }
+
+    public virtual void Exit()
+    {
+        Debug.Log("I eixt " + animBoolName);
+    }
+
+}
